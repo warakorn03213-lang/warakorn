cb51917c15e2876d9f98b53b6f5d39a50899d5ca
warakorn <user@warakorn.local>
2026-06-16 16:48:54 +0700

    connect

--- api/index.js
+++ api/index.js
@@ -1,2 +1,16 @@
-const app = require('../server/index.js');
+let app;
+try {
+  app = require('../server/index.js');
+} catch (err) {
+  // If server module fails to load, return the actual error for diagnosis
+  const express = require('express');
+  app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({
+      error: 'Server module failed to load',
+      message: err.message,
+      stack: err.stack
+    });
+  });
+}
 module.exports = app;
